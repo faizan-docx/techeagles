207d5f65b5872118979c20c93b6aee25fc38182c
faizan-docx <faizanahmad2020.fa@gmail.com>
2025-09-06 04:24:57 +0530

    final capital

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -288,7 +288,7 @@ const Footer = ({ isDarkMode }) => {
             whileHover={{ scale: 1.05 }}
             transition={{ type: "spring", stiffness: 400 }}
           >
-            Mahakumbrix entrepreneurship
+            MAHAKUMBRIX ENTREPRENEURSHIP
           </motion.div>
           
           {/* Floating Particles */}
